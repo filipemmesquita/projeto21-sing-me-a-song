337a30dcb57c314b8f4ff638a78cd6e5879a932b
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-09-26 05:49:23 -0300

    test: add e2e tests

--- front-end/cypress/e2e/newRecommendation.cy.js
+++ front-end/cypress/e2e/newRecommendation.cy.js
@@ -1,25 +1,70 @@
-import {faker} from '@faker-js/faker'
-import recommendationFactory from './factories/recommendationFactory.js'
+import { faker } from "@faker-js/faker";
+import * as scenarioFactory from "./factories/scenarioFactory.js";
+
 beforeEach(async () => {
-    await cy.request('POST', `${Cypress.env('baseURL')}/test/reset`, {});
+  await cy.request("POST", `${Cypress.env("baseURL")}/test/reset`, {});
+});
+
+describe("Tests homepage", () => {
+  it("Should add a recommendation", async () => {
+    const newRec = scenarioFactory.newRecommendation();
+
+    cy.visit("http://localhost:3000");
+
+    cy.get("input[placeholder=Name]").type(newRec.name);
+    cy.get("input[placeholder='https://youtu.be/...']").type(
+      newRec.youtubeLink
+    );
+
+    cy.intercept("POST", `${Cypress.env("baseURL")}/recommendations`).as(
+      "newRec"
+    );
+
+    cy.get("button").click();
+
+    cy.wait("@newRec");
+
+    cy.get("article").should("have.length", 1);
   });
+  it("Should add an upvote ", async () => {
+    await scenarioFactory.registerNewRecommendation();
 
-describe('Tests route POST/recommendations',()=>{
-    it('Tests if a new recommendation can be successfully created',async()=>{
-        const newRec=await recommendationFactory();
+    cy.visit("http://localhost:3000");
 
-        cy.visit('http://localhost:3000')
+    cy.contains("0").should("be.visible");
+    cy.get("article").first().find("svg").first().click();
+    cy.contains("1").should("be.visible");
+  });
+  it("Should subtract a downvote", async () => {
+    await scenarioFactory.registerNewRecommendation();
 
-        cy.get('input[placeholder=Name]').type(newRec.name);
-        cy.get("input[placeholder='https://youtu.be/...']").type(newRec.youtubeLink);
-        
+    cy.visit("http://localhost:3000");
 
-        cy.intercept('POST','http://localhost:4001/recommendations').as('newRec')
+    cy.contains("0").should("be.visible");
+    cy.get("article").first().find("svg").last().click();
+    cy.contains("-1").should("be.visible");
+  });
+});
+describe("Tests random page", () => {
+  it("Should show one random recommendation", async () => {
+    await scenarioFactory.registerNewRecommendation();
 
-        cy.get('button').click();
+    cy.visit("http://localhost:3000/");
+    cy.contains("Random").click();
 
-        cy.wait('@newRec');
+    cy.url().should("equal", "http://localhost:3000/random");
+    cy.get("article").should("have.length", 1);
+  });
+});
+describe("Tests top page", () => {
+  it("Should show 10 recommendations", async () => {
+    for (let x = 0; x < 10; x++) {
+      scenarioFactory.registerNewRecommendation();
+    }
+    cy.visit("http://localhost:3000/");
+    cy.contains("Top").click();
 
-        cy.get('article').should('have.length',1);
-    })
-})
+    cy.url().should("equal", "http://localhost:3000/top");
+    cy.get("article").should("have.length", 10);
+  });
+});
